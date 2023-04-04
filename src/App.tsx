1af48dbc3ed2caf0208982f79935da9a5f50d85e
Hasegawa-Akito <h.akito1999@gmail.com>
2023-04-04 17:28:19 +0900

    三角形デザイン

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import ImageCrip from "./components/ImageCrip";
 import Map from "./components/Map";
 import Predict from "./components/Predict";
 import Parent from "./components/UseCallback";
+import Bar from "./lib/Bar";
 
 const App = () => {
   return (
     <div className="App">
-      <ImageCrip />
+      <div className="m-10">
+        <Bar></Bar>
+      </div>
     </div>
   );
 };
---
+++ src/lib/Bar/index.tsx
@@ -0,0 +1,9 @@
+const Bar = () => {
+  return (
+    <div className="w-8 h-96 bg-blue-500 relative">
+      <div className="w-8 h-8 absolute top-10 left-2 z-10 border-[16px] border-transparent border-r-[16px] border-r-red-600"></div>
+    </div>
+  );
+};
+
+export default Bar;
