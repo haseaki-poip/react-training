6cff88091da51c8a5b8a5cd847fc9b30cf210dfb
Hasegawa-Akito <h.akito1999@gmail.com>
2023-04-04 18:05:07 +0900

    押した位置への移動を実装

--- src/lib/Bar/index.tsx
+++ src/lib/Bar/index.tsx
@@ -1,8 +1,32 @@
+import { MouseEvent, useState } from "react";
+
 const Bar = () => {
+  const halfSizeCursor = 16;
+
+  const [absolutePositionY, setAbsolutePositionY] = useState(-halfSizeCursor);
+
+  const moveCursorByClick = (e: React.MouseEvent<HTMLElement>) => {
+    const barPositionY = e.currentTarget.getBoundingClientRect().y;
+    const clickPositionOnPageY = e.pageY;
+    const clickPositionOnBarY = clickPositionOnPageY - barPositionY;
+    setAbsolutePositionY(clickPositionOnBarY - halfSizeCursor);
+  };
   return (
-    <div className="w-8 h-96 bg-blue-500 relative">
-      <div className="w-8 h-8 absolute top-10 left-2 z-10 border-[16px] border-transparent border-r-[16px] border-r-red-600"></div>
-    </div>
+    <>
+      <style>
+        {`
+          .cursor-position-top {
+            top: ${absolutePositionY}px;
+          }
+        `}
+      </style>
+      <div
+        onClick={(e) => moveCursorByClick(e)}
+        className="w-8 h-96 bg-blue-500 relative"
+      >
+        <div className="w-8 h-8 absolute cursor-position-top left-2 z-10 border-[16px] border-transparent border-r-[16px] border-r-red-600"></div>
+      </div>
+    </>
   );
 };
 
