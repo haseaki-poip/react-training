d2f7fb26f506c67c613b20ec7ac6e26f7eb8865d
Hasegawa-Akito <h.akito1999@gmail.com>
2023-04-06 23:42:23 +0900

    barの動きと数値を連動

--- src/lib/Bar/index.tsx
+++ src/lib/Bar/index.tsx
@@ -1,22 +1,37 @@
-import { MouseEvent, useState } from "react";
+import { ChangeEvent, MouseEvent, useCallback, useState } from "react";
 
 const Bar = () => {
   const halfSizeCursor = 16;
+  const maxInputNum = 100;
 
   const [absolutePositionY, setAbsolutePositionY] = useState({
     pre: -halfSizeCursor,
     now: -halfSizeCursor,
   });
+  const [inputNmber, setInputNumber] = useState(
+    absolutePositionY.now + halfSizeCursor
+  );
 
   const moveCursorByClick = (e: React.MouseEvent<HTMLElement>) => {
     const barPositionY = e.currentTarget.getBoundingClientRect().y;
     const clickPositionOnPageY = e.pageY;
     const clickPositionOnBarY = clickPositionOnPageY - barPositionY;
+    const now = clickPositionOnBarY - halfSizeCursor;
     setAbsolutePositionY({
       pre: absolutePositionY.now,
       now: clickPositionOnBarY - halfSizeCursor,
     });
+
+    setInputNumber(now + halfSizeCursor);
   };
+
+  const onEdit = (e: ChangeEvent<HTMLInputElement>) => {
+    const number = Number(e.target.value);
+    if (number > 100) return setInputNumber(100 - halfSizeCursor);
+    if (number < 0) return setInputNumber(-halfSizeCursor);
+    return setInputNumber(number);
+  };
+
   return (
     <>
       <style>
@@ -45,6 +60,14 @@ const Bar = () => {
         className="w-8 h-96 bg-blue-500 relative cursor-pointer"
       >
         <div className="w-8 h-8 absolute cursor-position-top left-2 z-10 border-[16px] border-transparent border-r-[16px] border-r-red-600"></div>
+      </div>
+      <div className="mt-5">
+        <input
+          value={inputNmber}
+          onChange={(e) => onEdit(e)}
+          type="number"
+          className="w-56 h-32 border-2 border-black text-center text-6xl"
+        />
       </div>
     </>
   );
