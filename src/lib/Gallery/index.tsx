700032bd0810a66dc2241e417e4e575b03f3a71e
Hasegawa-Akito <h.akito1999@gmail.com>
2023-04-26 17:57:56 +0900

    下部に黒グラデーションを実装

--- src/lib/Gallery/index.tsx
+++ src/lib/Gallery/index.tsx
@@ -1,8 +1,8 @@
 const Gallery = () => {
   return (
-    <div className="h-96 overflow-hidden my-10 px-5">
-      <div className="columns-2 md:columns-3">
-        <div className="w-full break-inside-avoid bg-black mb-2 h-36"></div>
+    <div className="h-[60vh] overflow-hidden my-10 relative">
+      <div className="bg-black columns-2 md:columns-3 px-5 py-10">
+        <div className="w-full break-inside-avoid bg-white mb-2 h-36"></div>
         <div className="w-full break-inside-avoid bg-red-400 mb-2 h-28"></div>
         <div className="w-full break-inside-avoid bg-blue-500 mb-2 h-40"></div>
         <div className="w-full break-inside-avoid bg-green-400 mb-2 h-32"></div>
@@ -13,8 +13,9 @@ const Gallery = () => {
         <div className="w-full break-inside-avoid bg-yellow-400 mb-2 h-24"></div>
         <div className="w-full break-inside-avoid bg-blue-400 mb-2 h-56"></div>
         <div className="w-full break-inside-avoid bg-gray-400 mb-2 h-48"></div>
-        <div className="w-full break-inside-avoid bg-black mb-2 h-32"></div>
+        <div className="w-full break-inside-avoid bg-white mb-2 h-32"></div>
       </div>
+      <div className="w-full h-24 opacity-90 bg-gradient-to-b from-transparent to-black absolute left-0 bottom-0 z-10"></div>
     </div>
   );
 };
