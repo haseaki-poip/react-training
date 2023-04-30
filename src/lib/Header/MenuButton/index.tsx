9bd300142fe18ff170e2521a1e11afbbd63abe94
Hasegawa-Akito <h.akito1999@gmail.com>
2023-04-30 17:25:49 +0900

    menuボタンの変形アニメーション実装

---
+++ src/lib/Header/MenuButton/index.tsx
@@ -0,0 +1,28 @@
+import { memo } from "react";
+
+const MenuButton = memo(({ isOpen }: { isOpen: boolean }) => {
+  return (
+    <div className="w-10 h-14 relative">
+      <i
+        className={
+          "bg-white w-full h-0.5 absolute left-1/2 -translate-x-1/2 -translate-y-1/2 transition-all ease-out duration-300 " +
+          (isOpen ? "top-1/2 rotate-45" : "top-1/4 rotate-0")
+        }
+      ></i>
+      <i
+        className={
+          "bg-white w-full h-0.5 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity ease-out duration-300 " +
+          (isOpen ? "opacity-0" : "opacity-100")
+        }
+      ></i>
+      <i
+        className={
+          "bg-white w-full h-0.5 absolute left-1/2 -translate-x-1/2 -translate-y-1/2 transition-all ease-out duration-300 " +
+          (isOpen ? "top-1/2 -rotate-45" : "top-3/4 rotate-0")
+        }
+      ></i>
+    </div>
+  );
+});
+
+export default MenuButton;
--- src/lib/Header/index.tsx
+++ src/lib/Header/index.tsx
@@ -1,16 +1,19 @@
+import { useState } from "react";
+import MenuButton from "./MenuButton";
+
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="fixed left-0 top-0 z-50 w-full h-20 bg-[#161616]">
       <div className="w-full h-full flex">
-        <div className="w-14 h-14 my-auto mx-auto">
+        <div className="w-14 h-14 my-auto mx-auto cursor-pointer">
           <img className="w-full h-full" src="/logo192.png" id="logo" alt="" />
         </div>
-        <div className="absolute left-8 top-1/2 z-50 -translate-y-1/2 text-white">
-          <div className="w-10 h-14 relative">
-            <i className="bg-white w-full h-0.5 absolute top-1/4 left-1/2 -translate-x-1/2 -translate-y-1/2"></i>
-            <i className="bg-white w-full h-0.5 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></i>
-            <i className="bg-white w-full h-0.5 absolute top-3/4 left-1/2 -translate-x-1/2 -translate-y-1/2"></i>
-          </div>
+        <div
+          className="absolute left-8 top-1/2 z-50 -translate-y-1/2 text-white cursor-pointer"
+          onClick={() => setIsOpen((prevIsOpen) => !prevIsOpen)}
+        >
+          <MenuButton isOpen={isOpen} />
         </div>
       </div>
     </div>
