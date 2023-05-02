848ff118e00df8c29e29da0e06075ee03d87566f
Hasegawa-Akito <h.akito1999@gmail.com>
2023-05-02 21:31:34 +0900

    メニューの位置関係変更

--- src/lib/Header/SideMenu/SubMenu/index.tsx
+++ src/lib/Header/SideMenu/SubMenu/index.tsx
@@ -9,18 +9,20 @@ const SubMenu = memo(({ isOpenSubMenu, handleBackButton }: Props) => {
   return (
     <div
       className={
-        "w-full md:w-96 h-screen absolute top-full z-50 md:z-30 bg-[#232323] border-t-[1px] border-t-white transition-all linear duration-500 " +
-        (isOpenSubMenu ? "left-0 md:left-96" : "-left-full md:-left-96")
+        "w-full md:w-96 h-screen absolute top-0 z-50 md:-z-30 transition-all linear duration-500 " +
+        (isOpenSubMenu ? "left-0 md:left-full" : "-left-full md:left-0")
       }
     >
-      <div className="text-white">
-        <ul>
-          <li onClick={handleBackButton}>back</li>
-          <li>Home</li>
-          <li>About us</li>
-          <li>Design Tent</li>
-          <li>Contact</li>
-        </ul>
+      <div className="bg-[#232323] w-full h-full">
+        <div className="text-white">
+          <ul>
+            <li onClick={handleBackButton}>back</li>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Design Tent</li>
+            <li>Contact</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/lib/Header/SideMenu/index.tsx
+++ src/lib/Header/SideMenu/index.tsx
@@ -14,29 +14,31 @@ const SideMenu = memo(({ isOpenMenu }: { isOpenMenu: boolean }) => {
     <>
       <div
         className={
-          "w-full md:w-96 h-screen absolute top-full z-40 bg-[#161616] border-t-[1px] border-t-white transition-all linear duration-500 " +
+          "w-full md:w-96 h-screen absolute top-full z-40 border-t-[1px] transition-all linear duration-500 " +
           (isOpenMenu ? "left-0" : "-left-full md:-left-96")
         }
       >
-        <div className="text-white">
-          <ul>
-            <li>Home</li>
-            <li>About us</li>
-            <li
-              onClick={() =>
-                setIsOpenSubmenu((prevIsOpenSubMenu) => !prevIsOpenSubMenu)
-              }
-            >
-              Design Tent
-            </li>
-            <li>Contact</li>
-          </ul>
+        <div className="bg-[#161616] border-t-white w-full h-full">
+          <div className="text-white">
+            <ul>
+              <li>Home</li>
+              <li>About us</li>
+              <li
+                onClick={() =>
+                  setIsOpenSubmenu((prevIsOpenSubMenu) => !prevIsOpenSubMenu)
+                }
+              >
+                Design Tent
+              </li>
+              <li>Contact</li>
+            </ul>
+          </div>
         </div>
+        <SubMenu
+          isOpenSubMenu={isOpenSubMenu}
+          handleBackButton={() => setIsOpenSubmenu(false)}
+        />
       </div>
-      <SubMenu
-        isOpenSubMenu={isOpenSubMenu}
-        handleBackButton={() => setIsOpenSubmenu(false)}
-      />
     </>
   );
 });
